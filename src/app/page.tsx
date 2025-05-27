b6caf59450e106c38fa2561d02a5a327be6fc2b5
Chewsday <z647623@gmail.com>
2025-05-27 13:23:35 +0800

    setup shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
+import ModeToggle from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <SignedOut>
-        <SignInButton mode='modal' />
+        <SignInButton mode='modal'>
+          <Button className="cursor-pointer">
+            Sign in
+          </Button>
+        </SignInButton>
       </SignedOut>
       
+      <ModeToggle />
       <SignedIn>
         <UserButton />
       </SignedIn>
