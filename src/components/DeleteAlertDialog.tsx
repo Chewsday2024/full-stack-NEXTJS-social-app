e087969f85d41d09a6c45c585eeb16c54469c55f
Chewsday <z647623@gmail.com>
2025-05-29 17:44:32 +0800

    post section done

---
+++ src/components/DeleteAlertDialog.tsx
@@ -0,0 +1,84 @@
+import { Loader2Icon, Trash2Icon } from "lucide-react"
+
+
+import { Button } from "./ui/button"
+import {
+  AlertDialogDescription,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialog
+} from "./ui/alert-dialog"
+
+
+
+
+
+type DeleteAlertDialogProps = {
+  isDeleting: boolean
+  onDelete: () => Promise<void>
+  title?: string
+  description?: string
+}
+
+
+
+
+function DeleteAlertDialog({
+  isDeleting,
+  onDelete,
+  title = 'Delete Post',
+  description = 'This action cannot be undone.'
+}: DeleteAlertDialogProps) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button
+          variant={'ghost'}
+          size={'sm'}
+          className="text-muted-foreground hover:text-red-500 -mr-2 cursor-pointer"
+        >
+          {isDeleting
+            ? (
+              <Loader2Icon className="size-4 animate-spin" />
+            )
+            : (
+              <Trash2Icon className="size-4" />
+            )
+          }
+        </Button>
+      </AlertDialogTrigger>
+
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            {title}
+          </AlertDialogTitle>
+
+          <AlertDialogDescription>
+            {description}
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <AlertDialogFooter>
+          <AlertDialogCancel className="cursor-pointer">
+            Cancel
+          </AlertDialogCancel>
+
+          <AlertDialogAction
+            onClick={onDelete}
+            className="bg-red-500 hover:bg-red-600 cursor-pointer"
+            disabled={isDeleting}
+          >
+            {isDeleting ? 'Deleting...' : 'Delete'}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}
+export default DeleteAlertDialog
